c376500bac7e2a2416a121b83a9ef8e62f2e47fe
ExcellaUser <ExcellaUser@gmail.com>
2016-01-31 13:57:06 -0800

    changes.

--- ExcellaMobileApp/Models/Result.cs
+++ ExcellaMobileApp/Models/Result.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization;
 
 namespace ExcellaMobileApp.Models
 {
 
     public class Entrance
     {
+
+        [DataMember]
         public string ID { get; set; }
+
+        [DataMember]
         public string Name { get; set; }
-        public string StationCode1 { get; set; }
+
+            [DataMember]
+       public string StationCode1 { get; set; }
+
+        [DataMember]
         public string StationCode2 { get; set; }
+
+        [DataMember]
         public string Description { get; set; }
+
+        [DataMember]
         public double Lat { get; set; }
+
+        [DataMember(Name = "StationLongitude")]
         public double Lon { get; set; }
     }
 
+   
     public class RootObject
     {
         public List<Entrance> Entrances { get; set; }
